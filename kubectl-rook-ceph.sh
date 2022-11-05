4d382ff8376cf7016330d2e373f111e3de8df71d
Pratik Surve <prsurve@redhat.com>
2022-11-05 10:01:39 +0530

    debug

--- kubectl-rook-ceph.sh
+++ kubectl-rook-ceph.sh
@@ -783,7 +783,7 @@ function run_dr_health() {
   # run ceph status command form one cluster to another
   echo
   info_msg "running ceph status from peer cluster"
-  set +e
+  set -x
   run_ceph_command -s --mon-host "$MON_HOST" --id rbd-mirror-peer --key "$PEER_KEY" "$@"
   exitCode=$?
   if [ $exitCode -ne 0 ]; then
@@ -792,7 +792,7 @@ function run_dr_health() {
   # run rbd pool mirror status
   info_msg "running mirroring daemon health"
   run_rbd_command -p "$blockpool_name" mirror pool status
-  set -e
+  set +x
 }
 
 ####################################################################################################
